cd5cbdfb671ab40ddf30fa47d6829d22b91de4c3
sajjad dehnavi <dehnavi671@gmail.com>
2023-08-18 19:56:46 +0330

    add password mode, bug fix paste mode, add parameter modifier, text style and ....

--- OtpTextField/src/main/java/dehnavi/sajjad/otptextfield/OtpTextField.kt
+++ OtpTextField/src/main/java/dehnavi/sajjad/otptextfield/OtpTextField.kt
@@ -1,19 +1,16 @@
 package dehnavi.sajjad.otptextfield
 
-import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -21,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
@@ -36,6 +34,7 @@ import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
@@ -43,12 +42,20 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun OtpTextField(
+    modifier: Modifier = Modifier,
+    modifierField: Modifier = Modifier,
     numField: Int = 4,
     value: String = "",
+    passwordChar: Char = '•',
+    isPasswordMode: Boolean = true,
+    textStyle: TextStyle = TextStyle.Default,
+    textSizePassword: TextUnit = 36.sp,
     borderShape: Shape = RoundedCornerShape(12.dp),
     borderColor: Color = Color.White,
     borderFocusColor: Color = Color.Blue,
@@ -56,6 +63,7 @@ fun OtpTextField(
     borderWidth: Dp = 1.dp,
     borderFocusWidth: Dp = 4.dp,
     isWrong: Boolean = true,
+    paddingFieldValue: Dp = 12.dp,
     onFinishedChange: ((String) -> Unit)? = null,
 ) {
     val localFocusManager = LocalFocusManager.current
@@ -93,23 +101,37 @@ fun OtpTextField(
     }
 
     LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(paddingFieldValue)
     ) {
         items(numField) { index ->
+            val colorBorderFocus = animateColorAsState(
+                targetValue = if (isWrong) {
+                    borderWrongColor
+                } else {
+                    if (index == focusIndex) borderFocusColor else borderColor
+                }, label = "COLOR"
+            )
+            val widthBorderFocus = animateDpAsState(
+                targetValue = if (index == focusIndex || isWrong) borderFocusWidth else borderWidth,
+                label = "WIDTH"
+            )
+
             ItemOtpField(
-                numField,
-                index,
-                isWrongState,
-                borderWrongColor,
-                focusIndex,
-                borderFocusColor,
-                borderColor,
-                borderFocusWidth,
-                borderWidth,
-                borderShape,
-                textOtp,
-                localFocusManager,
+                modifier = modifierField,
+                numField = numField,
+                index = index,
+                isWrong = isWrongState,
+                borderShape = borderShape,
+                textOtp = textOtp,
+                localFocusManager = localFocusManager,
+                widthBorder = widthBorderFocus.value,
+                colorBorder = colorBorderFocus.value,
+                passwordChar = passwordChar,
+                isPasswordMode = isPasswordMode,
                 disableWrongMode = { isWrongState = false },
+                textStyle = textStyle,
+                textSizePassword = textSizePassword,
                 onFocusChange = { newIndex ->
                     focusIndex = newIndex
                 },
@@ -124,18 +146,19 @@ fun OtpTextField(
 
 @Composable
 private fun ItemOtpField(
+    modifier: Modifier,
     numField: Int,
     index: Int,
     isWrong: Boolean,
-    borderWrongColor: Color,
-    focusIndex: Int,
-    borderFocusColor: Color,
-    borderColor: Color,
-    borderFocusWidth: Dp,
-    borderWidth: Dp,
     borderShape: Shape,
     textOtp: SnapshotStateList<String>,
     localFocusManager: FocusManager,
+    textStyle: TextStyle,
+    textSizePassword: TextUnit,
+    widthBorder: Dp,
+    colorBorder: Color,
+    passwordChar: Char,
+    isPasswordMode: Boolean,
     disableWrongMode: () -> Unit,
     onFocusChange: (Int) -> Unit,
     updateTextValue: (String) -> Unit,
@@ -144,68 +167,54 @@ private fun ItemOtpField(
     val isLastField = numField - 1 == index
     val isFirstField = index == 0
 
-    var textFieldValueState by remember {
-        mutableStateOf(
-            TextFieldValue(
-                text = textOtp[index]
-            )
+    val textFieldValueState =
+        TextFieldValue(
+            text = if (isPasswordMode) passwordChar.toString() else textOtp[index],
+            selection = TextRange(textOtp[index].length)
         )
-    }
 
-    val colorBorderFocus = animateColorAsState(
-        targetValue = if (isWrong) {
-            borderWrongColor
-        } else {
-            if (index == focusIndex) borderFocusColor else borderColor
-        }, label = "COLOR"
-    )
-    val widthBorderFocus = animateDpAsState(
-        targetValue = if (index == focusIndex || isWrong) borderFocusWidth else borderWidth,
-        label = "WIDTH"
-    )
-
-    BasicTextField(modifier = Modifier
+    BasicTextField(modifier = modifier
         .border(
-            width = widthBorderFocus.value, color = colorBorderFocus.value, shape = borderShape
+            width = widthBorder, color = colorBorder, shape = borderShape
         )
-        .width(40.dp)
-        .height(60.dp)
         .onFocusChanged {
             if (it.isFocused && it.hasFocus) {
                 //set focus
                 onFocusChange.invoke(index)
-                //set cursor selection
-                textFieldValueState =
-                    textFieldValueState.copy(selection = TextRange(textFieldValueState.text.length))
             }
         }
         .onKeyEvent { event: KeyEvent ->
             handleBackspaceKeyEvent(
-                textFieldValueState.text, event, isFirstField, localFocusManager
+                text = textFieldValueState.text,
+                event = event,
+                isFirstField = isFirstField,
+                localFocusManager = localFocusManager
             )
         },
         value = textFieldValueState,
         onValueChange = {
             handleValueChange(
-                textFieldValueState.text,
-                it.text,
-                numField,
-                localFocusManager,
-                isLastField,
-                isWrong,
-                disableWrongMode
+                oldValue = textOtp[index],
+                newValue = it.text,
+                numField = numField,
+                textOtp = textOtp,
+                localFocusManager = localFocusManager,
+                isLastField = isLastField,
+                isWrong = isWrong,
+                disableWrongMode = disableWrongMode
             ) { updatedValue ->
                 updateTextValue.invoke(updatedValue)
-
-                textFieldValueState = textFieldValueState.copy(text = updatedValue)
             }
 
             //all input filled
             if (checkIsAllInputFilled(textOtp)) {
-                onFinishedChange?.let { listener -> listener(textOtp.joinToString()) }
+                onFinishedChange?.let { listener -> listener(textOtp.joinToString("")) }
             }
         },
-        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+        textStyle = textStyle.copy(
+            textAlign = TextAlign.Center,
+            fontSize = if (isPasswordMode) textSizePassword else textStyle.fontSize
+        ),
         keyboardActions = KeyboardActions(onNext = {
             localFocusManager.moveFocus(
                 FocusDirection.Next
@@ -217,7 +226,11 @@ private fun ItemOtpField(
             keyboardType = KeyboardType.Decimal
         ),
         decorationBox = { innerTextField ->
-            Column(verticalArrangement = Arrangement.Center) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
                 innerTextField()
             }
         })
@@ -248,6 +261,7 @@ private fun handleValueChange(
     oldValue: String,
     newValue: String,
     numField: Int,
+    textOtp: SnapshotStateList<String>,
     localFocusManager: FocusManager,
     isLastField: Boolean,
     isWrong: Boolean,
@@ -259,18 +273,18 @@ private fun handleValueChange(
         disableWrongMode.invoke()
     }
     //handle paste mode
-    val pasteMode = (newValue.length - numField) == numField
+    val pasteMode = newValue.length == (numField + oldValue.length)
     if (pasteMode) {
-        newValue.forEachIndexed { charIndex, char ->
+        val pastedValue = newValue.subSequence(oldValue.length, newValue.length)
+        pastedValue.forEachIndexed { charIndex, char ->
             if (charIndex < numField) {
-                updateText.invoke(char.toString())
+                textOtp[charIndex] = char.toString()
 
-                if (charIndex < numField - 1) {
+                if (charIndex != numField - 1)
                     localFocusManager.moveFocus(FocusDirection.Next)
-                }
             }
         }
-    } else {
+    } else if (oldValue != newValue) {
         if (newValue.isEmpty()) {
             updateText.invoke(newValue)
         } else if (newValue.length == 1) {
@@ -279,18 +293,7 @@ private fun handleValueChange(
                 localFocusManager.moveFocus(FocusDirection.Next)
             }
         } else {
-            val lastChar = newValue.last().toString()
-            val firstChar = newValue.first().toString()
-
-            val newChar = if (oldValue == firstChar) lastChar else firstChar
-
-
-            Log.d(
-                "TAG",
-                "handleValueChange: new value $newValue  |||  first: $firstChar   ||| last: $lastChar  ||| new char: $newChar"
-            )
-
-            updateText.invoke(newChar)
+            updateText.invoke(newValue.last().toString())
 
             if (!isLastField) {
                 localFocusManager.moveFocus(FocusDirection.Next)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import dehnavi.sajjad.otptextfield.OtpTextField
 import kotlinx.coroutines.Dispatchers
@@ -37,9 +39,17 @@ class MainActivity : ComponentActivity() {
                     var isWrong by remember {
                         mutableStateOf(true)
                     }
-                    OtpTextField(borderColor = Color.Black, value = "1234", onFinishedChange = {
-                        Log.d("TAG", "onCreate: $it")
-                    }, isWrong = isWrong)
+                    OtpTextField(
+                        modifier = Modifier.fillMaxSize(),
+                        modifierField = Modifier.size(60.dp),
+                        borderColor = Color.Black,
+                        value = "1234",
+                        onFinishedChange = {
+                            Log.d("TAG", "onCreate: $it")
+                        },
+                        isWrong = isWrong,
+                        isPasswordMode = isWrong
+                    )
 
                     rememberCoroutineScope().launch(Dispatchers.IO) {
                         delay(5000)
@@ -50,7 +60,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
