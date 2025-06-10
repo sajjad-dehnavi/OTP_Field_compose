58ab328484a605ae9c0a763f7a499c7e242ee00f
mr dehnavi <dehnavi671@gmail.com>
2025-06-10 15:27:37 +0330

    add maven publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 21
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
